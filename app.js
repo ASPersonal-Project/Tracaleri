21d69b99dfdb8d6de036f2cae9ddcd92731c99c2
anjana6 <anjanashakthi95@gmail.com>
2019-09-07 19:39:38 +0530

    create item part

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const ItemCtrl = (function(){
     }
 
     const data = {
-        item : [ 
+        items : [ 
         {id:0, name:'Steak Dinner', calories: 1200},
         {id:1, name:'Cookie', calories: 400},
         {id:2, name:'Eggs', calories: 1000}
@@ -20,14 +20,35 @@ const ItemCtrl = (function(){
     }
 
     return {
+        getItems: function(){
+            return data.items;
+        },
         logData: function(){
             return data;
-        }
+        } 
     }
     
 })();
 
 const UICtrl = (function(){
+    const UISelector = {
+        itemList : '#item-list'
+
+    }
+
+
+    return{
+        populateItemList:function(items){
+            let html = '';
+
+            items.forEach(function(item){
+                html += `<li class="collection-list-item" id="item-${item.id}"><strong>${item.name} :</strong><em>${item.calories} Calories</em><a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a></li>`
+            });
+
+            document.querySelector(UISelector.itemList).innerHTML = html;
+        }
+    }
+    
     
 })();
 
@@ -36,8 +57,11 @@ const App = (function(ItemCtrl,UICtrl){
     //console.log(ItemCtrl.logData());
     return{
         init: function(){
-            console.log('hai');
+            //console.log('hai');
+            const items = ItemCtrl.getItems();
 
+            UICtrl.populateItemList(items);
+            //  
         }
     }
     
